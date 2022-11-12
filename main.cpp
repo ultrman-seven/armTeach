7607a9eaeb59fe50f8e0715319e9165938ef3ace
Leslie <1813180652@qq.com>
2022-11-12 20:50:17 +0800

    1

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "stdio.h"
+#include "stdint.h"
+
+
+int main(int argc, char const *argv[])
+{
+    uint8_t a;
+    uint16_t b;
+    uint32_t c;
+}
---
+++ regLED/Objects/main.d
@@ -0,0 +1,2 @@
+./objects/main.o: main.c \
+  D:\Program\ Files\arm\ keil\ARM\ARMCLANG\Bin\..\include\stdint.h
