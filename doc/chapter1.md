e96c236ef4ba9c7993e92ff835f30c0ea294e9da
Leslie <1813180652@qq.com>
2022-11-16 18:17:15 +0800

    update

--- doc/chapter1.md
+++ doc/chapter1.md
@@ -164,7 +164,52 @@ int main(int argc, char const *argv[])
 
 ```
 
-3. 联合体
+    **关于结构体的[内存对齐](https://blog.csdn.net/qq_22902757/article/details/106469793 "CSDN")**
+
+4. 联合体
+   ```c
+   #include "stdio.h"
+   #include "stdint.h"
+
+   typedef union
+   {
+       uint64_t u64Dat;
+       uint32_t u32Dat;
+       uint16_t u16Dat;
+       uint16_t u16Unit[8];
+       uint8_t u8Unit[8];
+   } tmpUnionDef;
+
+   int main(int argc, char const *argv[])
+   {
+       tmpUnionDef tmp;
+       uint64_t tmpAdd, d0Add, d1Add, d2Add, d3Add;
+       uint64_t num;
+       uint8_t *p;
+
+       tmpAdd = (uint64_t)&tmp;
+
+       d0Add = (uint64_t) & (tmp.u64Dat);
+       d1Add = (uint64_t) & (tmp.u32Dat);
+       d2Add = (uint64_t) & (tmp.u16Dat);
+       d3Add = (uint64_t)tmp.u16Unit;
+
+       printf("tmp:0x%x\n", *(uint32_t *)&tmpAdd);
+       printf("d0:0x%x\n", *(uint32_t *)&d0Add);
+       printf("d1:0x%x\n", *(uint32_t *)&d1Add);
+       printf("d2:0x%x\n", *(uint32_t *)&d2Add);
+       printf("d3:0x%x\n", *(uint32_t *)&d3Add);
+
+       tmp.u64Dat = 0x123456789abcdef;
+       num = tmp.u64Dat;
+       p = (uint8_t *)#
+
+       printf("u32=0x%x\n", tmp.u32Dat);
+       printf("u16=0x%x\n", tmp.u16Dat);
+       printf("u8 1:0x%x\n", tmp.u8Unit[1]);
+       printf("u8 1:0x%x", p[1]);
+   }
+   ```
 
 # 单片机
 
--- main.cpp
+++ main.cpp
@@ -1,41 +1,52 @@
 #include "stdio.h"
 #include "stdint.h"
 
-typedef union
+extern "C"
 {
-    uint64_t u64Dat;
-    uint32_t u32Dat;
-    uint16_t u16Dat;
-    uint16_t u16Unit[8];
-    uint8_t u8Unit[8];
-} tmpUnionDef;
+    typedef struct __treeNode treeNode;
+    struct __treeNode
+    {
+        int32_t value;
+        // treeNode *leftTree, *rightTree;
+    };
 
-int main(int argc, char const *argv[])
-{
-    tmpUnionDef tmp;
-    uint64_t tmpAdd, d0Add, d1Add, d2Add, d3Add;
-    uint64_t num;
-    uint8_t *p;
+    typedef struct __testStruct testStruct;
+    struct __testStruct
+    {
+        uint64_t v1;
+        struct __treeNode root;
+        union
+        {
+            uint64_t v2;
+            struct
+            {
+                uint32_t v2L;
+                uint32_t v2H;
+            };
+        };
+    };
 
-    tmpAdd = (uint64_t)&tmp;
+    // typedef struct
+    // {
+    //     testStruct *test;
+    //     chainNode *next;
+    // } chainNode;
 
-    d0Add = (uint64_t) & (tmp.u64Dat);
-    d1Add = (uint64_t) & (tmp.u32Dat);
-    d2Add = (uint64_t) & (tmp.u16Dat);
-    d3Add = (uint64_t)tmp.u16Unit;
+    int main(int argc, char const *argv[])
+    {
+        testStruct t;
+        uint32_t address1, address2;
 
-    printf("tmp:0x%x\n", *(uint32_t *)&tmpAdd);
-    printf("d0:0x%x\n", *(uint32_t *)&d0Add);
-    printf("d1:0x%x\n", *(uint32_t *)&d1Add);
-    printf("d2:0x%x\n", *(uint32_t *)&d2Add);
-    printf("d3:0x%x\n", *(uint32_t *)&d3Add);
+        printf("size of 'treeNode' is %d\n", sizeof(treeNode));
+        printf("size of 'testStruct' is %d\n", sizeof(testStruct));
 
-    tmp.u64Dat = 0x123456789abcdef;
-    num = tmp.u64Dat;
-    p = (uint8_t *)&num;
+        address1 = (uint32_t) & (t.v2);
+        address2 = (uint32_t) & (t.v2L);
+        printf("v2 and v2L: %d\n", address1 - address2);
 
-    printf("u32=0x%x\n", tmp.u32Dat);
-    printf("u16=0x%x\n", tmp.u16Dat);
-    printf("u8 1:0x%x\n", tmp.u8Unit[1]);
-    printf("u8 1:0x%x", p[1]);
+        printf("add of t is: 0x%x\n", (uint32_t)(&t));
+        printf("add of v1 is: 0x%x\n", (uint32_t)&(t.v1));
+        printf("add of root is: 0x%x\n", (uint32_t)&(t.root));
+        printf("add of v2 is: 0x%x\n", (uint32_t)&(t.v2));
+    }
 }
